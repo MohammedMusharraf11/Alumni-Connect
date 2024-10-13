a1fdbf8108572411509c678a22199fb57005e7a3
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-10-13 23:21:21 +0530

    reinstalled bcrypt:

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,5 +1,5 @@
 const User = require('../Models/users'); // Ensure you are importing User correctly
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { loginValidation } = require('../Middlewares/AuthValidation');
 
