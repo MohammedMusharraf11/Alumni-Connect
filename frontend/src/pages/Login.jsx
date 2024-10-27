30f38b9dc477d2ad64acedc8ac9b55503eb3aac9
prashantsingh181 <prashant.singh181@gmail.com>
2024-10-27 12:46:23 +0530

    fix: added layout with margin for all screen sizes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { handleError, handleSuccess } from '../utils/utils'; // Import your utility functions
 import { ToastContainer } from 'react-toastify';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+
 
 function Login() {
     const [loginInfo, setLoginInfo] = useState({
@@ -69,75 +68,71 @@ function Login() {
 
     return (
         <>
-            <div className='sm:mx-[10%]'>
-                <Navbar />
-                <div className="container mx-auto py-16 text-outfit">
-                    <div className="max-w-md mx-auto p-8 border border-[#D4D4D4] rounded-lg shadow-lg bg-white">
-                        <h3 className="text-3xl font-bold text-center text-secondary mb-4">Login</h3>
-                        <p className="text-zinc-500 text-center mb-5">Please login to connect with Alumni or Students</p>
+            <div className="container mx-auto py-16 text-outfit">
+                <div className="max-w-md mx-auto p-8 border border-[#D4D4D4] rounded-lg shadow-lg bg-white">
+                    <h3 className="text-3xl font-bold text-center text-secondary mb-4">Login</h3>
+                    <p className="text-zinc-500 text-center mb-5">Please login to connect with Alumni or Students</p>
 
-                        {/* User Type Selection */}
-                        <div className="flex justify-center mb-4">
-                            <label className="mr-4">
-                                <input
-                                    type="radio"
-                                    value="student"
-                                    checked={userType === 'student'}
-                                    onChange={handleUserTypeChange}
-                                />
-                                Student
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    value="alumni"
-                                    checked={userType === 'alumni'}
-                                    onChange={handleUserTypeChange}
-                                />
-                                Alumni
-                            </label>
-                        </div>
-
-                        <form onSubmit={handleSubmit}>
-                            <div className="flex flex-col mb-4">
-                                <label htmlFor="collegeEmail" className="text-gray-600 mb-2">Email</label>
-                                <input
-                                    type="text" // Changed from 'collegeEmail' to 'text'
-                                    id="collegeEmail"
-                                    name="collegeEmail"
-                                    value={loginInfo.collegeEmail}
-                                    onChange={handleChange}
-                                    className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary-500"
-                                />
-                            </div>
-                            <div className="flex flex-col mb-4">
-                                <label htmlFor="password" className="text-gray-600 mb-2">Password</label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    value={loginInfo.password}
-                                    onChange={handleChange}
-                                    className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary-500"
-                                />
-                            </div>
-                            <div className="flex justify-center mt-4">
-                                <button type="submit" className="bg-primary text-white px-4 py-2 rounded-md hover:bg-primary-600">
-                                    Login
-                                </button>
-                            </div>
-                        </form>
-                        <br />
-                        <p className="text-gray-700 text-center">
-                            Don't have an account?{' '}
-                            <Link to="/roleselection" className="text-blue-500 hover:underline">
-                                Register Here
-                            </Link>
-                        </p>
+                    {/* User Type Selection */}
+                    <div className="flex justify-center mb-4">
+                        <label className="mr-4">
+                            <input
+                                type="radio"
+                                value="student"
+                                checked={userType === 'student'}
+                                onChange={handleUserTypeChange}
+                            />
+                            Student
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                value="alumni"
+                                checked={userType === 'alumni'}
+                                onChange={handleUserTypeChange}
+                            />
+                            Alumni
+                        </label>
                     </div>
-                    <ToastContainer />
+
+                    <form onSubmit={handleSubmit}>
+                        <div className="flex flex-col mb-4">
+                            <label htmlFor="collegeEmail" className="text-gray-600 mb-2">Email</label>
+                            <input
+                                type="text" // Changed from 'collegeEmail' to 'text'
+                                id="collegeEmail"
+                                name="collegeEmail"
+                                value={loginInfo.collegeEmail}
+                                onChange={handleChange}
+                                className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                            />
+                        </div>
+                        <div className="flex flex-col mb-4">
+                            <label htmlFor="password" className="text-gray-600 mb-2">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={loginInfo.password}
+                                onChange={handleChange}
+                                className="border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                            />
+                        </div>
+                        <div className="flex justify-center mt-4">
+                            <button type="submit" className="bg-primary text-white px-4 py-2 rounded-md hover:bg-primary-600">
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                    <br />
+                    <p className="text-gray-700 text-center">
+                        Don't have an account?{' '}
+                        <Link to="/roleselection" className="text-blue-500 hover:underline">
+                            Register Here
+                        </Link>
+                    </p>
                 </div>
-                <Footer />
+                <ToastContainer />
             </div>
         </>
     );
